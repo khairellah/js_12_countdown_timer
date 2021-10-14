e42a48629fcd4c0cf1bfd5e50e7d39d31bc90f59
khairellah <60810477+khairellah@users.noreply.github.com>
2021-10-14 12:24:14 +0100

    add js code part 1 until 5:24:00

--- app.js
+++ app.js
@@ -21,3 +21,25 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+ // Get  Elements
+ const giveaway = document.querySelector(".giveaway");
+ const deadline = document.querySelector(".deadline");
+ const items = document.querySelectorAll(".deadline-format h4");
+ //console.log(items); show all my info days, hours, min, sec
+
+ let futureDate = new Date(2021,11,24,12,30,0); // Date() : gets the actual date
+// console.log(futureDate) //show date format
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month]
+const date = futureDate.getDate();
+
+const weekday = weekdays [futureDate.getDay()];
+// console.log(weekday)
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`
+
+
